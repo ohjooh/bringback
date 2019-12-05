367c6fa296ad42fec073ff0c7f20f110be42035b
jopopcorn <supop1009@gmail.com>
2019-12-05 15:10:15 +0900

    Update ChangeTasteActivity

--- app/src/main/java/com/example/user/projectbringback/ChangeTasteActivity.java
+++ app/src/main/java/com/example/user/projectbringback/ChangeTasteActivity.java
@@ -1,14 +1,121 @@
 package com.example.user.projectbringback;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChangeTasteActivity extends AppCompatActivity {
+    private List<String> tasteList = new ArrayList<>();
+    private SparseBooleanArray mSelectedItems = new SparseBooleanArray(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_taste);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_clear_black_24dp);
+        RecyclerView tasteView = findViewById(R.id.tastesView);
+        initTastes();
+
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.HORIZONTAL);
+        tasteView.setLayoutManager(gridLayoutManager);
+        final TasteAdapter adapter = new TasteAdapter(this, tasteList);
+        tasteView.setAdapter(adapter);
+
+        final Animation aniZoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+
+        adapter.setOnItemClickListener(new TasteAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(TasteAdapter.TasteViewHolder holder, View view, int position) {
+                if (mSelectedItems.get(position, false)) {
+                    mSelectedItems.delete(position);
+                    holder.tasteName.setTextColor(Color.WHITE);
+                    holder.tasteName.setBackgroundColor(Color.BLACK);
+                    int textHeight = holder.tasteName.getHeight();
+                    int textWidth = holder.tasteName.getWidth();
+                    holder.tasteName.setHeight(textHeight - 50);
+                    holder.tasteName.setWidth(textWidth - 50);
+                } else {
+                    if (mSelectedItems.size() >= 2) {
+                        Toast.makeText(ChangeTasteActivity.this, "최대 두 가지 취향만 선택할 수 있습니다.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        mSelectedItems.put(position, true);
+                        holder.tasteName.setTextColor(getColor(R.color.colorPrimary));
+                        holder.tasteName.setBackgroundColor(getColor(R.color.colorSecondary));
+                        int textHeight = holder.tasteName.getHeight();
+                        int textWidth = holder.tasteName.getWidth();
+                        holder.tasteName.setHeight(textHeight + 50);
+                        holder.tasteName.setWidth(textWidth + 50);
+                        holder.tasteName.startAnimation(aniZoomIn);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_submit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else if (item.getItemId() == R.id.submit) {
+            if(mSelectedItems.size() == 0){
+                Toast.makeText(this, "좋아하는 음악 취향을 한 가지 이상 선택해주세요.", Toast.LENGTH_SHORT).show();
+            }else{
+                Log.d("SELECTED ITEM","selected item ? "+mSelectedItems);
+                for(int i=0;i<tasteList.size();i++){
+                    Log.d("mSelectedItems","index of key "+mSelectedItems.get(i));
+                    if(mSelectedItems.get(i)){
+                        //TODO 여기에서 i값의 tasteName를 User에 저장하기
+                    }
+                }
+                finish();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initTastes() {
+        tasteList.add("발라드");
+        tasteList.add("댄스");
+        tasteList.add("팝");
+        tasteList.add("포크/어쿠스틱");
+        tasteList.add("아이돌");
+        tasteList.add("랩/힙합");
+        tasteList.add("알앤비/소울");
+        tasteList.add("일렉트로닉");
+        tasteList.add("락/메탈");
+        tasteList.add("재즈");
+        tasteList.add("인디");
+        tasteList.add("OST");
+        tasteList.add("클래식");
+        tasteList.add("뉴에이지");
+        tasteList.add("레게");
+        tasteList.add("블루스");
+        tasteList.add("기타");
     }
 }
---
+++ app/src/main/java/com/example/user/projectbringback/TasteAdapter.java
@@ -0,0 +1,74 @@
+package com.example.user.projectbringback;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TasteAdapter extends RecyclerView.Adapter<TasteAdapter.TasteViewHolder> {
+    private Context context;
+    private List<String> tastes;
+    private OnItemClickListener listener;
+
+    public interface OnItemClickListener {
+        public void onItemClick(TasteViewHolder holder, View view, int position);
+    }
+
+    public TasteAdapter(Context context, List<String> taste) {
+        this.context = context;
+        this.tastes = taste;
+    }
+
+    public class TasteViewHolder extends RecyclerView.ViewHolder {
+        TextView tasteName;
+        OnItemClickListener listener;
+
+        public TasteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tasteName = itemView.findViewById(R.id.tasteName);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null)
+                        listener.onItemClick(TasteViewHolder.this, v, position);
+                }
+            });
+        }
+
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+    }
+
+    @NonNull
+    @Override
+    public TasteAdapter.TasteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.taste, parent, false);
+        return new TasteViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TasteAdapter.TasteViewHolder holder, int position) {
+        holder.tasteName.setText(tastes.get(position));
+        holder.setOnItemClickListener(listener);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return tastes.size();
+    }
+
+}
